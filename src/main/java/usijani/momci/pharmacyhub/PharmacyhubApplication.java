4b5300d8c5b8042fa87e9391eb5fbae96012de62
begnoo <zoranjankov99@gmail.com>
2021-03-21 15:18:41 +0100

    fix: changed git ignore file

--- src/main/java/usijani/momci/pharmacyhub/PharmacyhubApplication.java
+++ src/main/java/usijani/momci/pharmacyhub/PharmacyhubApplication.java
@@ -1,16 +1,7 @@
 package usijani.momci.pharmacyhub;
 
-import java.util.Collections;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.Ordered;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class PharmacyhubApplication {
