41a67d562f366cfca2e455a5e270f134e3ab5448
begnoo <zoranjankov99@gmail.com>
2021-03-21 16:08:13 +0100

    fix: fixed cors restrictions and front end client initialization

---
+++ src/main/java/pharmacyhub/PharmacyhubApplication.java
@@ -0,0 +1,22 @@
+package pharmacyhub;
+
+import java.util.Collections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@SpringBootApplication
+public class PharmacyhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmacyhubApplication.class, args);
+	}
+	
+}	
--- src/main/java/pharmacyhub/controllers/TestController.java
+++ src/main/java/pharmacyhub/controllers/TestController.java
@@ -1,6 +1,5 @@
-package usijani.momci.pharmacyhub;
+package pharmacyhub.controllers;
 
-import java.awt.List;
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -13,20 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/test")
-public class Test {
+public class TestController {
 	
-	/*
-	 * Prilikom poziva metoda potrebno je navesti nekoliko parametara
-	 * unutar @@GetMapping anotacije: url kao vrednost 'value' atributa (ukoliko se
-	 * izostavi, ruta do metode je ruta do kontrolera), u slucaju GET zahteva
-	 * atribut 'produce' sa naznakom tipa odgovora (u nasem slucaju JSON).
-	 * 
-	 * Kao povratna vrednost moze se vracati klasa ResponseEntity koja sadrzi i telo
-	 * (sam podatak) i zaglavlje (metapodatke) i status kod, ili samo telo ako se
-	 * metoda anotira sa @ResponseBody.
-	 * 
-	 * url: /api/greetings GET
-	 */
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Collection<String>> getGreetings() {
 		Collection<String> greetings = new ArrayList<String>();
