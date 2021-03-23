f5520c0e50272e778b3f9fc362be96a850809541
matkoman-py <62108859+matkoman-py@users.noreply.github.com>
2021-03-23 11:21:40 +0100

    feat: added a search component in the patient display, just the frontend

--- frontend-client/src/views/PatientOverview.vue
+++ frontend-client/src/views/PatientOverview.vue
@@ -1,16 +1,32 @@
 <template>
-    <PatientTable/>
+    <b-container>
+        <b-row>
+            <b-col>
+                <PatientTable/>
+            </b-col>
+        </b-row>
+        <b-row align-h="center">
+            <b-col sm="3" md="4" lg="4">
+                <SearchPatient/>
+            </b-col>
+        </b-row>
+        
+    </b-container>
 </template>
 
 <script>
 import PatientTable from "@/components/PatientTable"
+import SearchPatient from "@/components/SearchPatient"
 export default {
     components:{
-        PatientTable
+        PatientTable,
+        SearchPatient
     }
 }
 </script>
 
 <style>
-
+#nesting_div{
+    text-align: center;
+}
 </style>
