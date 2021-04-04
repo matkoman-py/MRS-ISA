d00b7e2dd0f676cbd76dedd68752572d73f057a8
begnoo <zoranjankov99@gmail.com>
2021-04-04 20:27:01 +0200

    feat: added footer

--- frontend-client/src/App.vue
+++ frontend-client/src/App.vue
@@ -3,7 +3,13 @@
     
     <navbar></navbar>
     <router-view />
+    <footer class="bg-light text-center text-lg-start">
 
+    <div class="text-center p-3">
+      © 2021 Copyright:
+      <a class="text-dark"><i>Usijani Momci (Nekromantija)</i></a>
+    </div>
+  </footer>
   </div>
 </template>
 
