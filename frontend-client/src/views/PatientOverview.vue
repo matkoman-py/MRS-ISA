8cf7799a61527b7582bf3631c494342b1768977b
Aleksandar Cepic <aleksandar.epic@gmail.com>
2021-04-05 17:44:04 +0000

    Feature:Patient profile information change added.

--- frontend-client/src/views/PatientOverview.vue
+++ frontend-client/src/views/PatientOverview.vue
@@ -1,83 +1,79 @@
 <template>
     <b-container>
+        <b-modal id="my-modal" title="Your profile" hide-footer>
+            <b-form @submit="onSubmit">
+                <b-form-group label="Name" label-for="name-input" invalid-feedback="Name is required">
+                    <b-form-input id="name-input" v-model="selected.name"></b-form-input>
+                </b-form-group>
+
+                <b-form-group label="Surame" label-for="surname-input" invalid-feedback="Surname is required">
+                    <b-form-input id="surname-input" v-model="selected.surname"></b-form-input>
+                </b-form-group>
+
+                <b-form-group label="E-mail" label-for="email-input" invalid-feedback="E-mail is required">
+                    <b-form-input id="email-input" type="email" v-model="selected.email"></b-form-input>
+                </b-form-group>
+
+                <b-form-group label="Phone number" label-for="phonenumber-input"
+                    invalid-feedback="Phone number is required">
+                    <b-form-input id="phonenumber-input" v-model="selected.phoneNumber" ></b-form-input>
+                </b-form-group>
+                <b-button type="submit" variant="primary">Save</b-button>
+                <b-button type="button" variant="danger" @click="handleClose">Cancel</b-button>
+            </b-form>
+        </b-modal>
+
         <b-row>
             <b-col>
-                <b-table id="tabela_pacijenata" striped hover :items="patients"></b-table>
+                <b-table id="tabela_pacijenata" striped hover :items="patients" @row-clicked="showModal"></b-table>
             </b-col>
         </b-row>
         <b-row align-h="center">
             <b-col sm="3" md="4" lg="4">
                 <div>
                     <b-form @submit="search" v-if="show">
-                    <!-- <b-form-group
-                        id="input-group-1"
-                        label="Email address:"
-                        label-for="input-1"
-                    >
-                        <b-form-input
-                        id="input-1"
-                        v-model="form.email"
-                        type="email"
-                        placeholder="Enter email"
-                        required
-                        ></b-form-input>
-                    </b-form-group> -->
+                        <b-form-group id="input-group-2" label="Patient Name:" label-for="input-2">
+                            <b-form-input id="input-2" v-model="form.name" placeholder="Enter name"></b-form-input>
+                        </b-form-group>
 
-                    <b-form-group id="input-group-2" label="Patient Name:" label-for="input-2">
-                        <b-form-input
-                        id="input-2"
-                        v-model="form.name"
-                        placeholder="Enter name"
-                        ></b-form-input>
-                    </b-form-group>
-                    
-                    <b-form-group id="input-group-4" label="Patient Surname:" label-for="input-4">
-                        <b-form-input
-                        id="input-4"
-                        v-model="form.surname"
-                        placeholder="Enter surname"
-                        ></b-form-input>
-                    </b-form-group>
-
-                    <!-- <b-form-group id="input-group-3" label="Category:" label-for="input-3">
-                        <b-form-select
-                        id="input-3"
-                        v-model="form.food"
-                        :options="foods"
-                        required
-                        ></b-form-select>
-                    </b-form-group> -->
-
-
-                    <b-button type="submit" variant="primary">Search</b-button>
+                        <b-form-group id="input-group-4" label="Patient Surname:" label-for="input-4">
+                            <b-form-input id="input-4" v-model="form.surname" placeholder="Enter surname">
+                            </b-form-input>
+                        </b-form-group>
+                        <b-button type="submit" variant="primary">Search</b-button>
                     </b-form>
-                    
+
                 </div>
             </b-col>
         </b-row>
-        
+
     </b-container>
 </template>
 
 <script>
-import axios from "axios";
-export default {
-    
-    data: function() {
-        return{
-        patients:[],
-        form: {
-          email: '',
-          name: '',
-          surname: '',
-          food: null
+    import axios from "axios";
+    export default {
+
+        data: function () {
+            return {
+                selected: {},
+                modified: {},
+                patients: [],
+                form: {
+                    email: '',
+                    name: '',
+                    surname: '',
+                    food: null
+                },
+                foods: [{
+                    text: 'Select One',
+                    value: null
+                }, 'Bronze', 'Silver', 'Gold'],
+                show: true
+            }
         },
-        foods: [{ text: 'Select One', value: null }, 'Bronze', 'Silver', 'Gold'],
-        show: true
-        }
-    },
-    methods: {
-      search: function() {
+        methods: {
+            search: function () {
                 axios.get('http://localhost:8081/patients/search', {
                         params: {
                             patientNameParam: this.form.name,
@@ -91,17 +87,17 @@ export default {
                                 surname: patient.surname,
                                 email: patient.email,
                                 phoneNumber: patient.phoneNumber,
-                                adress:  patient.location? patient.location.address : "Null",
-                                country: patient.location? patient.location.country:"Null",
-                                city: patient.location? patient.location.city:"Null",
+                                adress: patient.location ? patient.location.address : "Null",
+                                country: patient.location ? patient.location.country : "Null",
+                                city: patient.location ? patient.location.city : "Null",
                                 points: patient.points,
                                 category: patient.category,
                             }));
                     })
                     .catch(error => console.log(error));
-        },
-        getAllPatients: function(){
-            axios.get('http://localhost:8081/patients')
+            },
+            getAllPatients: function () {
+                axios.get('http://localhost:8081/patients')
                     .then(response => {
                         this.patients = response.data.map(patient =>
                             ({
@@ -109,25 +105,49 @@ export default {
                                 surname: patient.surname,
                                 email: patient.email,
                                 phoneNumber: patient.phoneNumber,
-                                adress:  patient.location? patient.location.address : "Null",
-                                country: patient.location? patient.location.country:"Null",
-                                city: patient.location? patient.location.city:"Null",
+                                adress: patient.location ? patient.location.address : "Null",
+                                country: patient.location ? patient.location.country : "Null",
+                                city: patient.location ? patient.location.city : "Null",
                                 points: patient.points,
                                 category: patient.category,
                             }));
                     })
                     .catch(error => console.log(error));
-            
+
+            },
+            onSubmit(event) {
+                event.preventDefault();
+                this.modified.name = this.selected.name;
+                this.modified.surname = this.selected.surname;
+                this.modified.email = this.selected.email;
+                this.modified.phoneNumber = this.selected.phoneNumber;
+                this.$root.$emit('bv::hide::modal', 'my-modal');
+                console.log(this.modified);
+                axios.put("http://localhost:8081/patients", this.modified)
+                    .then(response => {
+                        console.log(response);
+                        console.log("ovde");
+                    })
+                    .catch(error => console.log(error));
+            },
+            handleClose() {
+                this.$root.$emit('bv::hide::modal', 'my-modal');
+            },
+            showModal(item) {
+                this.$root.$emit('bv::show::modal', 'my-modal');
+                this.selected = JSON.parse(JSON.stringify(item));
+                this.modified = item;
+            }
+        },
+
+        mounted: function () {
+            this.getAllPatients();
         }
-    },
-    mounted: function (){
-        this.getAllPatients();
     }
-}
 </script>
 
 <style>
-#nesting_div{
-    text-align: center;
-}
+    #nesting_div {
+        text-align: center;
+    }
 </style>
