305be35e042559a1bc4351dd60ccf10946a5ef73
Zoran <zoranjankov99@gmail.com>
2021-05-10 14:11:05 +0200

    feat: added confirmation code to drug reservation and reservation notification

--- src/main/java/pharmacyhub/domain/DrugReservation.java
+++ src/main/java/pharmacyhub/domain/DrugReservation.java
@@ -13,8 +13,8 @@ import pharmacyhub.domain.users.Patient;
 
 @Entity
 @SQLDelete(sql = "UPDATE drug_price SET deleted = true WHERE id = ?")
-public class DrugReservation extends BaseEntity{
-	
+public class DrugReservation extends BaseEntity {
+
 	@ManyToOne
 	@JoinColumn(name = "drug_fk")
 	private Drug drug;
@@ -24,14 +24,17 @@ public class DrugReservation extends BaseEntity{
 	@Column(nullable = false)
 	private int amount;
 	@OneToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name = "patient_id",nullable = true)
-	private Patient patient; //bice patient objekat
+	@JoinColumn(name = "patient_id", nullable = true)
+	private Patient patient; // bice patient objekat
 	@Column(nullable = false)
 	private String date;
-	
+	@Column
+	private String confirmationCode;
+
 	public DrugReservation() {
 		super();
 	}
+
 	public DrugReservation(Drug drug, Drugstore drugstore, int amount, Patient patient, String date) {
 		super();
 		this.drug = drug;
@@ -40,37 +43,53 @@ public class DrugReservation extends BaseEntity{
 		this.patient = patient;
 		this.date = date;
 	}
-	
+
 	public String getDate() {
 		return date;
 	}
+
 	public void setDate(String date) {
 		this.date = date;
 	}
+
 	public Drug getDrug() {
 		return drug;
 	}
+
 	public void setDrug(Drug drug) {
 		this.drug = drug;
 	}
+
 	public Drugstore getDrugstore() {
 		return drugstore;
 	}
+
 	public void setDrugstore(Drugstore drugstore) {
 		this.drugstore = drugstore;
 	}
+
 	public int getAmount() {
 		return amount;
 	}
+
 	public void setAmount(int amount) {
 		this.amount = amount;
 	}
+
 	public Patient getPatient() {
 		return patient;
 	}
+
 	public void setPatient(Patient patient) {
 		this.patient = patient;
 	}
-	
-	
+
+	public String getConfirmationCode() {
+		return confirmationCode;
+	}
+
+	public void setConfirmationCode(String confirmationCode) {
+		this.confirmationCode = confirmationCode;
+	}
+
 }
