410fbc18ba0bc01c4897d73abbb1bca6bded293c
Zoran Jankov <zoranjankov99@gmail.com>
2021-05-17 21:59:27 +0200

    refactor: redesigned qr scanner form

--- frontend-client/src/components/QrCodeScanner.vue
+++ frontend-client/src/components/QrCodeScanner.vue
@@ -1,55 +1,91 @@
 <template>
     <div>
-        {{qrResult}}
-         <form @submit="scanImage">
-            <label for="img">Select image:</label>
-            <input ref="qrContainer" type="file" id="img" name="img" accept="image/*">
-            <input type="submit" value="Get Drugstores">
-        </form> 
+        <form @submit="scanImage">
+            <b-form-row>
+                <b-col style="height: 300px">
+                    <div id="preview">
+                        <img
+                            v-if="url"
+                            :src="url"
+                            height="300px"
+                            width="300px"
+                        />
+                        <img
+                            v-else
+                            height="300px"
+                            width="300px"
+                            src="https://pest-mart.com/wp-content/plugins/woo-category-slider-by-pluginever/assets/images/placeholder.png"
+                        />
+                    </div>
+                </b-col>
+                <b-col>
+                    <h3 style="text-align: left;">eReceipt Scanner</h3>
+                    <b-form-row class="mt-3">
+                        <label>Select image:</label>
+                    </b-form-row>
+                    <b-form-row class="mt-3">
+                        <b-form-file
+                            ref="qrContainer"
+                            type="file"
+                            id="img"
+                            name="img"
+                            accept="image/*"
+                            variant="outline-hub"
+                            @change="onImageChange"
+                        ></b-form-file>
+                    </b-form-row>
+                    <b-form-row class="mt-3">
+                        <b-button type="submit" variant="outline-hub">
+                            Get Drugstores
+                        </b-button>
+                    </b-form-row>
+                </b-col>
+            </b-form-row>
+        </form>
     </div>
 </template>
 
 <script>
-    import QrScanner from 'qr-scanner';
-    import QrScannerWorkerPath from '!!file-loader!../../node_modules/qr-scanner/qr-scanner-worker.min.js';
-    QrScanner.WORKER_PATH = QrScannerWorkerPath;
+import QrScanner from "qr-scanner";
+import QrScannerWorkerPath from "!!file-loader!../../node_modules/qr-scanner/qr-scanner-worker.min.js";
+QrScanner.WORKER_PATH = QrScannerWorkerPath;
 
-    export default {
-        name: "QrCodeScanner",
-        components: {
-            
-        },
-        computed: {
+export default {
+    name: "QrCodeScanner",
+    components: {},
+    computed: {},
 
+    data: function() {
+        return {
+            drugstores: [],
+            qrResult: {},
+            url: "",
+        };
+    },
+    methods: {
+        onImageChange: function(event) {
+            const file = event.target.files[0];
+            this.url = URL.createObjectURL(file);
         },
-
-        data: function () {
-            return {
-                drugstores: [],
-                qrResult: {},
-            }
-        },
-        methods: {
-            scanImage: function(event){
-                event.preventDefault();
-                const image = this.$refs['qrContainer'].files[0];
-                QrScanner.scanImage(image)
-                .then(result => 
-                {
+        scanImage: function(event) {
+            event.preventDefault();
+            const image = this.$refs["qrContainer"].files[0];
+            QrScanner.scanImage(image)
+                .then((result) => {
                     this.qrResult = this.parseResult(result);
                     this.$emit("qr-code-scanned", this.qrResult);
                 })
-                .catch(error => console.log(error || 'No QR code found.'));
-            },
-            parseResult: function(qrCode){
-                const parts = JSON.parse(qrCode).split(";");
-                const result = {};
-                for(let part of parts){
-                    const [key, amount] = part.split(',');
-                    result[key] = amount;
-                }
-                return result;
-            }
+                .catch((error) => console.log(error || "No QR code found."));
         },
-    }
+        parseResult: function(qrCode) {
+            const parts = JSON.parse(qrCode).split(";");
+            const result = {};
+            for (let part of parts) {
+                const [key, amount] = part.split(",");
+                result[key] = amount;
+            }
+            return result;
+        },
+    },
+};
 </script>
---
+++ frontend-client/src/views/eReceipt/ReceiptScannerView.vue
@@ -0,0 +1,82 @@
+<template>
+    <b-container>
+        <b-row class="mt-3">
+            <b-col>
+                <b-card>
+                    <qr-code-scanner
+                        v-on:qr-code-scanned="getReceiptDrugstores"
+                    ></qr-code-scanner>
+                </b-card>
+                <b-card class="mt-3">
+                    <b-table
+                        v-if="drugstores.length != 0"
+                        head-variant="outline-hub"
+                        striped
+                        hover
+                        :fields="fields"
+                        :items="drugstores"
+                        sticky-header="400px"
+                    ></b-table>
+                    <div v-else>
+                        There are no drugstores which contain all those drugs.
+                    </div>
+                </b-card>
+            </b-col>
+        </b-row>
+    </b-container>
+</template>
+
+<script>
+import { mapState } from "vuex";
+import QrCodeScanner from "../../components/QrCodeScanner";
+
+export default {
+    name: "ReceiptGeneratorView",
+    components: {
+        QrCodeScanner,
+    },
+    computed: {
+        ...mapState({
+            user: (state) => state.userModule.loggedInUser,
+            email: (state) => state.userModule.loggedInUser.email,
+            role: (state) => state.userModule.loggedInUser.type,
+        }),
+    },
+
+    data: function() {
+        return {
+            drugstores: [],
+            fields: [
+                {
+                    key: "name",
+                },
+                {
+                    key: "description",
+                },
+                {
+                    key: "averageRating",
+                },
+                {
+                    key: "workingHoursFrom",
+                },
+                {
+                    key: "workingHoursTo",
+                },
+            ],
+        };
+    },
+    methods: {
+        getReceiptDrugstores: function(receipt) {
+            this.$http
+                .post(`http://localhost:8081/drugstores/search-receipt`, {
+                    receiptData: receipt,
+                })
+                .then((response) => {
+                    this.drugstores = response.data;
+                    console.log(response.data);
+                })
+                .catch((error) => console.log(error));
+        },
+    },
+};
+</script>
