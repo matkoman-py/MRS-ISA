79e616f8b6d8fbe52b85b8aa5e2f88952400bd0a
Zoran Jankov <zoranjankov99@gmail.com>
2021-05-18 21:38:49 +0200

    fix: edit drugstores crud fix

---
+++ frontend-client/src/components/forms/drugstore/EditDrugstoreForm.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <drugstore-basic-info :form="form"></drugstore-basic-info>
+    </div>
+</template>
+
+<script>
+import DrugstoreBasicInfo from "../../DrugstoreBasicInfo.vue";
+export default {
+    props: ["form"],
+    components: { DrugstoreBasicInfo },
+    data: function() {
+        return {};
+    },
+    methods: {
+        getSelectedDrugstoreInfo: function() {},
+        submitHandler(event) {
+            event.preventDefault();
+            console.log(this.form);
+            alert("Update saved.");
+        },
+    },
+    mounted: function() {
+        this.getSelectedDrugstoreInfo();
+    },
+};
+</script>
