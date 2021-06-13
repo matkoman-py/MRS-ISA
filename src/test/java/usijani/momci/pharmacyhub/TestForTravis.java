b471bb528cd7361b82e00ff23968ddf432d4ba61
matkoman-py <62108859+matkoman-py@users.noreply.github.com>
2021-06-13 17:12:56 +0200

    feat: deploying

--- src/test/java/usijani/momci/pharmacyhub/TestForTravis.java
+++ src/test/java/usijani/momci/pharmacyhub/TestForTravis.java
@@ -7,8 +7,11 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import pharmacyhub.PharmacyhubApplication;
+
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = { PharmacyhubApplication.class })
 public class TestForTravis {
 
 	@Test
