71a7a0ef9b81961e266e2e6dd92a0c335b612760
matkoman-py <62108859+matkoman-py@users.noreply.github.com>
2021-06-13 17:06:46 +0200

    feat: deploying

---
+++ src/test/java/usijani/momci/pharmacyhub/TestForTravis.java
@@ -0,0 +1,19 @@
+package usijani.momci.pharmacyhub;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestForTravis {
+
+	@Test
+	void test() {
+		assertTrue(true);
+	}
+
+}
