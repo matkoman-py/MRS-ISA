ba9f8ce106be366c9c0edd443e9646fd744bf8d9
matkoman-py <62108859+matkoman-py@users.noreply.github.com>
2021-06-13 17:24:07 +0200

    feat: travis ci

--- src/test/java/usijani/momci/pharmacyhub/TestForTravis.java
+++ src/test/java/usijani/momci/pharmacyhub/TestForTravis.java
@@ -3,15 +3,15 @@ package usijani.momci.pharmacyhub;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import pharmacyhub.PharmacyhubApplication;
 
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = { PharmacyhubApplication.class })
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 public class TestForTravis {
 
 	@Test
